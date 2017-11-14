2a6a0b6d106f07baff32b6254daace77659685b7
Vitalik Lavrenko <vitaliklavrenko@MacBook-Pro-Vitalik.local>
2017-11-14 23:54:52 +0200

    fix task Closure

--- main.js
+++ main.js
@@ -1,51 +1,34 @@
-//Homework 6.1//
-function value() {
 
-  var input = document.querySelector('input[type="search"]')
-  var text = " this your length"
-  var resultId = document.getElementById('result')
 
-  var getNumberOfSymbvol = function() {
-   this.res = input // this.res - is diferent 
-      return this.res.value.length
-         
-  }
+function footballTeams(teamName, championshipName, placeTeam) {
 
- var apendRes = function () {
-    input.addEventListener('keypress', function(event) {
-      if (event.keyCode === 13) {
-          this.res = resultId
-          this.input = input
+    var name = teamName
+    var championship = championshipName
+    var place = placeTeam 
+    
+    
+    this.getName = function () {
+        console.log(name)
+    }
 
-          this.number = getNumberOfSymbvol()
-          this.text = text
-          this.res.innerHTML = this.number + this.text 
-          this.input.value = ''
-      }
-    }, false)
-  }
+    this.getChampionship = function () {
+        console.log(championship)
+    }
 
-  apendRes()
+    this.getPlace = function () {
+        console.log(place)
+    }
+    return function () {
+        console.log('Team name : ' + name)
+        console.log('Name championship : ' + championship)
+        console.log('Place team : ' + place)
+    }
+
 }
 
-value()
+var footballTeams = new footballTeams('Dynamo Kyiv', 'Ukrainian Premier League', '2')
+footballTeams()
 
-//Homework 6.2//
 
-function debounce (callback, time) {
-  var timer
 
-  return function () {
-    var args = arguments
-    clearTimeout(timer)
-    timer = setTimeout(function () {
-      callback(args[0])
-    }, time)
-  }
-}
 
-document
-  .querySelector('input[type="search"]')
-  .addEventListener('input', debounce(function (event) {
-    console.log(event.target.value)
-  }, 250), false)
